eaad4afee7fe06cd7d519196a7852055810477da
Devansh Baghel <devanshbaghel85@gmail.com>
2025-07-26 23:18:48 +0530

    feat: central auth server works

--- auth-server/src/lib/better-auth/index.ts
+++ auth-server/src/lib/better-auth/index.ts
@@ -3,6 +3,7 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { betterAuth } from 'better-auth';
 import { betterAuthOptions } from './options';
+import { user, account, session, verification } from "../../db/schema";
 
 /**
  * Better Auth Instance
@@ -13,9 +14,28 @@ export const auth = (env: CloudflareBindings): ReturnType<typeof betterAuth> => 
 
   return betterAuth({
     ...betterAuthOptions,
-    database: drizzleAdapter(db, { provider: 'pg' }),
+    database: drizzleAdapter(db, {
+      provider: "pg",
+      schema: {
+        user,
+        session,
+        account,
+        verification,
+      },
+    }),
     baseURL: env.BETTER_AUTH_URL,
     secret: env.BETTER_AUTH_SECRET,
+    socialProviders: {
+      github: {
+        clientId: env.GITHUB_CLIENT_ID,
+        clientSecret: env.GITHUB_CLIENT_SECRET,
+      },
+      google: {
+        clientId: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+      },
+    },
+    trustedOrigins: ["http://localhost:3000"],
 
     // Additional options that depend on env ...
   });
